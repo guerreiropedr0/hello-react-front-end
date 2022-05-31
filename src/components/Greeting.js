d99174b9a477db8dcbef05f011b4a4c8fdafd279
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-05-31 11:42:23 +0100

    Add redux setup and display random greeting in Greeting component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,5 +1,20 @@
-const Greeting = () => (
-  <div>Greetings me lorde</div>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import fetchGreetings from '../redux/greetings/greetings';
+
+const Greeting = () => {
+  const message = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+  }, []);
+
+  return (
+    <h1>
+      {message}
+    </h1>
+  );
+};
 
 export default Greeting;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import Greeting from './modules/Greeting';
+import Greeting from './components/Greeting';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/greetings" element={<Greeting />} />
-    </Routes>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/greetings" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>,
 );
